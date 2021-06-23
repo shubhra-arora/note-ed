e7e3f3ebbd12bf2a0162db949e7893002b5270af
Shubhra <62790710+shubhra-arora@users.noreply.github.com>
2021-06-24 00:57:08 +0530

    Add files via upload

---
+++ UpdateTaskServlet.java
@@ -0,0 +1,50 @@
+package com.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.DAO.taskDao;
+import com.Db.DBConnect;
+import com.entities.task;
+
+
+@WebServlet("/update")
+public class UpdateTaskServlet extends HttpServlet {
+	
+
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException,IOException
+	{
+		int id = Integer.parseInt(req.getParameter("id"));
+		String taskDao = req.getParameter("task");
+		String status = req.getParameter("status");
+		
+		taskDao dao = new taskDao(DBConnect.getConn());
+		
+		task t=new task();
+		t.setId(id);
+		t.setTask(taskDao);
+		t.setStatus(status);
+		
+		boolean f = dao.updateTask(t);
+		
+		HttpSession session = req.getSession();
+		
+		if(f) {
+			session.setAttribute("sucMsg",  "Task Updated Successfully");;
+			resp.sendRedirect("task-form.jsp");
+		}else {
+			session.setAttribute("failedMsg" , "Something wrong on server");
+			System.out.println("task-form.jsp");
+					
+		}
+		
+	}
+}
